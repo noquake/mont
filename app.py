4e6fa633154669237bb57d0e89b94cd8b46dcd64
W-Geedorah <kwakuagyapong478@gmail.com>
2025-11-01 12:06:56 -0400

    starting initializer and main functions

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import argparse
 
 def get_state(dev_dir="/dev", state_file="orig_state"):
     dev_path = Path(dev_dir)
-    state_path = Path.home() / "mont/.state" / state_file
+    orig_state = Path.home() / "mont/.state" / orig_state
 
     #  checking if path to directory exists
-    state_path.parent.mkdir(parents=True, exist_ok=True)
+    orig_state.parent.mkdir(parents=True, exist_ok=True)
 
     # Get all partition devices (e.g., sdb1, sdc1)
     partitions = [partition.name for partition in dev_path.iterdir() if partition.name.startswith("sd")]
-    state_path.write_text("\n".join(sorted(partitions)))
-    print(f"Baseline device state saved to {state_path}")
+    orig_state.write_text("\n".join(sorted(partitions)))
+    print(f"Baseline device state saved to {orig_state}")
     return set(partitions)
 
 def initialize():
@@ -24,6 +24,20 @@ def initialize():
         elif inp == "n":
             orig_state = Path.home() / "mont/.state/orig_state.txt"
 
+def check_and_link(dev_dir="/dev", state_file="current_state"):
+    dev_path = Path(dev_dir)
+    orig_state = Path.home() / "mont/.state" / orig_state
+    current_state = Path.home() / "mont/.state" / current_state
+    mount_dir = Path.home() / "mont" 
+
+    try:
+        orig_devices = set(orig_state.read_text().splitlines())
+    except FileNotFoundError:
+        print(f"Error:{orig_state} not found. Run 'mont init' first.")
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(
         program = "mont",
@@ -34,6 +48,15 @@ def main():
     # subcommand: mont init
     subparsers.add_parser("init", help = "Initialize and save baseline device /dev directory state")
     
-    # subcommand: mont check
-    subparsers.add_parser("check", help = "Compare current device state to baseline")
+    if args.command == "init":
+        print("Initializing...")
+    
+    if args.command == "mount":
+        check_and_link()
+    
+    else:
+        check_and_link() 
 
+# TODO: finish check_and_link
+# TODO: get_state() needs to be more flexibly/reusable
+# TODO: learn how to package the code -> install system wide on MAMMON
